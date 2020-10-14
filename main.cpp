ba108a892e2ee5550b68dce7c336b43edaf912bd
dxdx9988 <72845532+dxdx9988@users.noreply.github.com>
2020-10-14 12:00:48 +0800

    Update main.cpp
    main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
 
+int i18n=0;
+
 int main(int argc,const char** argv)
 {
+
+    const char* i18nFile="./i18n/en";
+    switch(i18n)
+    {
+    case 1:
+    case 2:
+    default:
+    break;    
+    }
+
+    FILE* f = fopen(i18nFile,"r");
+    if(f){
+        
+        fclose(f);
+    }
+
 return 0;
 }
